1901bc0abe1124948931ec08464b8e29830cf281
ilmasheva-eliza <ilmeliza36@gmai.com>
2026-01-31 10:56:52 +0500

    Разработанный пользовательский элемент управления. Ilmasheva

---
+++ pr20_ilma/Classes/GroupContext.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using pr20_ilma.Classes.Common;
+using pr20_ilma.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr20_ilma.Classes
+{
+    public class GroupContext : Group
+    {
+        public GroupContext(int Id, string Name) : base(Id, Name) { }
+        public static List<GroupContext> AllGroups()
+        {
+            List<GroupContext> allGroups = new List<GroupContext>();
+            MySqlConnection connection = Connection.OpenConnection();
+            MySqlDataReader BDGroups = Connection.Query("SELECT * FROM `group` ORDER BY `Name`", connection);
+            while (BDGroups.Read())
+            {
+                allGroups.Add(new GroupContext(
+                BDGroups.GetInt32(0),
+                BDGroups.GetString(1)));
+            }
+            Connection.CloseConnection(connection);
+            return allGroups;
+        }
+    }
+}
