dab4c6cb76f79005f66cb4ce1ceb283d5b784bf6
ilmasheva-eliza <ilmeliza36@gmai.com>
2026-01-31 09:59:59 +0500

    Разработанный класс подключения к БД. Фамилия.Но пока без бд».Ильмашева

---
+++ pr20_ilma/Classes/Common/Connection.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr20_ilma.Classes.Common
+{
+    public class Connection
+    {
+        public static string config = "server=127.0.0.1;uid=root;pwd=root;database=journal;";
+        public static MySqlConnection OpenConnection()
+        {
+            // Создаём подключение
+            MySqlConnection connection = new MySqlConnection(config);
+            // Открываем подключение
+            connection.Open();
+            // Возвращаем открытое подключение
+            return connection;
+        }
+    public static MySqlDataReader Query(string SQL, MySqlConnection connection)
+        {
+            return new MySqlCommand(SQL, connection).ExecuteReader();
+        }
+
+    public static void CloseConnection(MySqlConnection connection)
+        {
+            // Закрываем подключение
+            connection.Close();
+            // Очищаем пул подключений
+            MySqlConnection.ClearPool(connection);
+        }
+    }
+}
