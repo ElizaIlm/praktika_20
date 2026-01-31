ab9d404536def154fb3f172529a3662f7d218fe6
ilmasheva-eliza <ilmeliza36@gmai.com>
2026-01-31 09:46:29 +0500

    Разработанные модели проекта. Ilmasheva

---
+++ pr20_ilma/Models/Group.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr20_ilma.Models
+{
+    public class Group
+    {
+        int Id { get; set; }
+        public string Name { get; set; }
+        public Group(int Id, string Name)
+        {
+            this.Id = Id;
+            this.Name = Name;
+        }   
+    }
+}
